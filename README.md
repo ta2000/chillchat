3e2cb55c36f70917c722a4a7980536d53d3fb05e
Tom Andersen <tomandersen2000@gmail.com>
2018-01-11 21:07:05 -0800

    README: Install instructions
    Signed-off-by: Tom Andersen <tomandersen2000@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Chill Chat
 
 End to end encrypted picture messaging
+
+## Setup
+
+```console
+git clone https://github.com/pdxjohnny/chillchat
+cd chillchat
+./scripts/deps.sh
+cd app/
+python -m http.server 1234
+```
--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p app/libs/{css,js}
 wget -O app/libs/css/mui.min.css 'https://cdn.muicss.com/mui-0.9.35/css/mui.min.css'
 wget -O app/libs/js/mui.min.js 'https://cdn.muicss.com/mui-0.9.35/js/mui.min.js'
 wget -O app/libs/js/upup.min.js 'https://github.com/TalAter/UpUp/raw/master/dist/upup.min.js'
